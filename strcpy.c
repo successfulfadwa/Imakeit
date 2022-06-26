4a139718877d4f1f80860db6d823a986a8d447b4
successfulfadwa <fadwa.aljaoui@gmail.com>
2022-06-26 05:41:08 +0100

    strcpy2

--- strcpy.c
+++ strcpy.c
@@ -6,22 +6,31 @@
 /*   By: faljaoui <faljaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 05:23:16 by faljaoui          #+#    #+#             */
-/*   Updated: 2022/06/26 05:29:37 by faljaoui         ###   ########.fr       */
+/*   Updated: 2022/06/26 05:40:44 by faljaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h> 
 
-char *stpcpy(char * dst, char * src)
+char *ft_strcpy(char * dst, char * src)
 {
     int i ;
     i=0;
     while (src[i]!='\0')
     {
-        dst[i]== src[i];
+        dst[i]= src[i];
         i++;
     }
-    dst[i]=='\0';
+    dst[i]='\0';
     return(dst);
 }
+ int main ()
+ {
+     char s[]="fadwa";
+     char *d;
+     char *g;
+     g = ft_strcpy(d,s);
+     printf("%s",d);
+     
+ }

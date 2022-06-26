9a08c77422bd72fddcc456cdf738d4953b883830
successfulfadwa <fadwa.aljaoui@gmail.com>
2022-06-26 04:09:55 +0100

    first word complete

--- first_word.c
+++ first_word.c
@@ -6,15 +6,26 @@
 /*   By: faljaoui <faljaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 03:50:08 by faljaoui          #+#    #+#             */
-/*   Updated: 2022/06/26 03:53:37 by faljaoui         ###   ########.fr       */
+/*   Updated: 2022/06/26 04:07:10 by faljaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h> 
 
-int main [int argc , char **argv ]
+int main (int argc , char **argv )
 {
-    
-    
+    int i ;
+    i = 0;
+    if (argc == 2)
+    {
+    while (argv[1][i]== 32 || argv[1][i]==9)
+    i++;
+    while ((argv[1][i]!=32 && argv [1][i]!=9) && argv[1][i])
+    {
+        write (1,&argv[1][i],1);
+        i++;
+    }
+        write (1,"\n",1);
+    }
 }

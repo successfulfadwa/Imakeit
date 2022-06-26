2d00c67dc0491872cdc553880e7bccde3c27ef05
successfulfadwa <fadwa.aljaoui@gmail.com>
2022-06-26 05:50:34 +0100

    my_strlen + make main

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,20 +6,26 @@
 /*   By: faljaoui <faljaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 05:44:24 by faljaoui          #+#    #+#             */
-/*   Updated: 2022/06/26 05:47:08 by faljaoui         ###   ########.fr       */
+/*   Updated: 2022/06/26 05:49:53 by faljaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h> 
 
- size_t strlen(const char *s)
+ int ft_strlen(const char *s)
  {
-     int i
-     i =0;
-     while(s[i]!='\0')
+     int i ;
+     i = 0;
+     while(s[i] != '\0')
      {
          i++;
      }
      return (i);
+ }
+ int main ()
+ {
+     char *s="fadwa";
+     ft_strlen(s);
+     printf("%d",ft_strlen(s));
  }
